90737b91263b7bc50ec993fd2c684f65fe3ef585
24Syleo <sylvain.beggiora@outlook.com>
2023-06-01 18:01:14 +0200

    keydown ok

--- main.js
+++ main.js
@@ -49,3 +49,72 @@ function resultat() {
     let resultat = eval(equation); //eval est une fonction javascript eval('equation') elle evalue / execute la variable en parametre 
     document.getElementById('sortie').innerHTML = resultat; // le resulat est stocker dans la variable resultat plus qu'a afficher
 }
+
+//onkeydown evenement executer par le clavier
+document.addEventListener("keydown", (evt) => {
+    let key = evt.keyCode;
+    console.log(key);
+    switch (key) {
+        case 48:
+        case 96:
+            ajoutValeur(0);
+            break;
+        case 49:
+        case 97:
+            ajoutValeur(1);
+            break;
+        case 50:
+        case 98:
+            ajoutValeur(2);
+            break;
+        case 51:
+        case 99:
+            ajoutValeur(3);
+            break;
+        case 52:
+        case 100:
+            ajoutValeur(4);
+            break;
+        case 53:
+        case 101:
+            ajoutValeur(5);
+            break;
+        case 54:
+        case 102:
+            ajoutValeur(6);
+            break;
+        case 55:
+        case 103:
+            ajoutValeur(7);
+            break;
+        case 56:
+        case 104:
+            ajoutValeur(8);
+            break;
+        case 57:
+        case 105:
+            ajoutValeur(9);
+            break;
+        case 106:
+            ajoutValeur('*');
+            break;
+        case 107:
+            ajoutValeur('+');
+            break;
+        case 109:
+            ajoutValeur('-');
+            break;
+        case 110:
+            ajoutValeur('.');
+            break;
+        case 111:
+            ajoutValeur('/');
+            break;
+        case 13:
+            resultat();
+            break;
+        case 8:
+            forclear();
+            break;
+    }
+});
